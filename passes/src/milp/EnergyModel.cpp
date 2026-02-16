2438eec7f591b2cfd1cf0a5e49cff93c79c1662d
Byeongjee Kang <byeongjee.kang@gmail.com>
2026-02-16 11:31:45 -0500

    Parse optional loop_chunking_enabled from MILP config

--- passes/src/milp/EnergyModel.cpp
+++ passes/src/milp/EnergyModel.cpp
@@ -163,6 +163,15 @@ std::optional<MILPEnergyParams> parseMILPEnergyParams(const std::string &configP
     params.memRestoreEnergyPerByte = config["mem_restore_energy_per_byte"].get<double>();
     params.vmCapacityBytes = config["vm_capacity_bytes"].get<unsigned>();
     params.qRebootProb = config["q_reboot_probability"].get<double>();
+    params.loopChunkingEnabled = false;
+    if (config.contains("loop_chunking_enabled")) {
+        if (!config["loop_chunking_enabled"].is_boolean()) {
+            llvm::errs() << "Error: Field 'loop_chunking_enabled' must be boolean"
+                         << " in MILP config: " << configPath << "\n";
+            return std::nullopt;
+        }
+        params.loopChunkingEnabled = config["loop_chunking_enabled"].get<bool>();
+    }
 
     return params;
 }
