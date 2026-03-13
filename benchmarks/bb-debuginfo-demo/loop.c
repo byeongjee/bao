ed70bc6c510de78a4cd6b76ed79e942f3d48b6c4
Byeongjee Kang <byeongjee.kang@gmail.com>
2026-03-12 22:33:51 -0400

    Remove unused bb-debuginfo-demo directory

--- benchmarks/bb-debuginfo-demo/loop.c
+++
@@ -1,8 +0,0 @@
-// Loop: multiple basic blocks (entry, loop header, loop body, exit)
-int sum_to_n(int n) {
-    int sum = 0;
-    for (int i = 1; i <= n; i++) {
-        sum += i;
-    }
-    return sum;
-}
--- benchmarks/bb-debuginfo-demo/simple_branch.c
+++
@@ -1,7 +0,0 @@
-// Simple branching: 3 basic blocks (entry, if.then, if.else merged at return)
-int simple_branch(int x) {
-    if (x > 0) {
-        return x * 2;
-    }
-    return x + 1;
-}
